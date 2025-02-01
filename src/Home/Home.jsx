ae1eee06bf72004f3da794f4af2c1c743aeabdf4
tuhin4ever <tuhin4ever19@gmail.com>
2025-02-02 05:30:03 +0600

    feat: add Philosophy section and integrate About component; update dependencies

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,12 @@
+import About from "../Sections/About/About";
+
 const Home = () => {
   return (
     <div>
       <h1 className="text-3xl text-red-600 font-bold underline">
         Hello world!
       </h1>
+      <About />
     </div>
   );
 };
---
+++ src/Sections/Philosophy/Philosophy.jsx
@@ -0,0 +1,11 @@
+
+
+const Philosophy = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Philosophy;
